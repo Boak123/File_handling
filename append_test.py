7537b772ba637631b0cfff1ad8d47c731b600ac6
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-07 23:34:50 -0700

    corrections

--- append_test.py
+++ append_test.py
@@ -3,7 +3,7 @@ Write a program that:
 
 Asks the user to enter a sentence.
 Appends that sentence to the end of notes.txt.
-Ensures each new sentence starts on a new line.
+Ensures each new sentence starts on a new line
 """
 sentence = input("Enter a sentence: ")
 
